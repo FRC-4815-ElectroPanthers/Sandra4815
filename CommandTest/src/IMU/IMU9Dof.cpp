7ea8d78b2b6bccde62d8479f64294e94e205f712
MadScientistHououinKyouma <MadScientisHououinKyouma@users.noreply.github.com>
2016-01-03 03:35:37 -0500

    Added files for eventual Adafruit 9Dof IMU

---
+++ CommandTest/src/IMU/IMU9Dof.cpp
@@ -0,0 +1,18 @@
+/*
+ * IMU9Dof.cpp
+ *
+ *  Created on: Jan 3, 2016
+ *      Author: Phia
+ */
+
+#include "IMU9Dof.h"
+
+IMU9Dof::IMU9Dof() {
+	// TODO Auto-generated constructor stub
+
+}
+
+IMU9Dof::~IMU9Dof() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ CommandTest/src/IMU/IMU9Dof.h
@@ -0,0 +1,25 @@
+/*
+ * IMU9Dof.h
+ *
+ *  Created on: Jan 3, 2016
+ *      Author: Phia
+ */
+
+#ifndef IMU9DOF_H_
+#define IMU9DOF_H_
+
+#include <interfaces/Accelerometer.h>
+#include <I2C.h>
+#include <LiveWindow/LiveWindowSendable.h>
+#include <PIDSource.h>
+
+class IMU9Dof: public Accelerometer,
+		public I2C,
+		public LiveWindowSendable,
+		public PIDSource {
+public:
+	IMU9Dof();
+	virtual ~IMU9Dof();
+};
+
+#endif /* IMU9DOF_H_ */
