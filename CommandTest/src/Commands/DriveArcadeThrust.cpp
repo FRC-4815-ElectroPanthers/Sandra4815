28e6e833f984f5f49aa73d5992cc390813a8b323
MadScientistHououinKyouma <MadScientisHououinKyouma@users.noreply.github.com>
2016-03-03 01:03:40 -0500

    Fixed DriveARcadeThrust to use "drive" joystick variable instead of previous "joy1"

--- CommandTest/src/Commands/DriveArcadeThrust.cpp
+++ CommandTest/src/Commands/DriveArcadeThrust.cpp
@@ -16,7 +16,7 @@ void DriveArcadeThrust::Initialize()
 // Called repeatedly when this Command is scheduled to run
 void DriveArcadeThrust::Execute()
 {
-	CommandBase::drivetrain->ArcadeDriveThrust(oi->joy1->GetRawAxis(0), oi->joy1->GetRawAxis(3)+1.0,-1*oi->joy1->GetRawAxis(4)+1.0 );
+	CommandBase::drivetrain->ArcadeDriveThrust(oi->drive->GetRawAxis(0), oi->drive->GetRawAxis(3)+1.0,-1*oi->drive->GetRawAxis(4)+1.0 );
 }
 
 // Make this return true when this Command no longer needs to run execute()
