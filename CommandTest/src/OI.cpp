e9b3c6764b3750ee6253fdd1608a0564b54b4a98
MadScientistHououinKyouma <MadScientisHououinKyouma@users.noreply.github.com>
2016-03-13 19:25:42 -0400

    Commented out unused variables for possible later use

--- CommandTest/src/OI.cpp
+++ CommandTest/src/OI.cpp
@@ -13,13 +13,13 @@ OI::OI()
 	drive = new Joystick(0);
 	operate = new Joystick(1);
 
-	JoystickButton *drive1 = new JoystickButton(drive, 1),
-				   *drive2 = new JoystickButton(drive, 2),
-				   *drive3 = new JoystickButton(drive, 3),
-				   *drive4 = new JoystickButton(drive, 4),
+	JoystickButton //*drive1 = new JoystickButton(drive, 1),
+				   //*drive2 = new JoystickButton(drive, 2),
+				   //*drive3 = new JoystickButton(drive, 3),
+				   //*drive4 = new JoystickButton(drive, 4),
 				   *drive5 = new JoystickButton(drive, 5),
 				   *drive6 = new JoystickButton(drive, 6),
-				   *drive7 = new JoystickButton(drive, 7),
+				   //*drive7 = new JoystickButton(drive, 7),
 				   *drive8 = new JoystickButton(drive, 8);
 
 	drive5->WhenPressed(new DriveArcadeRampUp());
@@ -32,8 +32,8 @@ OI::OI()
 				   *operate4 = new JoystickButton(operate, 4),
 				   *operate5 = new JoystickButton(operate, 5),
 				   *operate6 = new JoystickButton(operate, 6),
-				   *operate7 = new JoystickButton(operate, 7),
-				   *operate8 = new JoystickButton(operate, 8);
+				   *operate7 = new JoystickButton(operate, 7)
+				   ;//,*operate8 = new JoystickButton(operate, 8);
 
 	operate1->WhenPressed(new DriveArmPosition(IntakeShooter::kInOutTake));
 	operate2->WhenPressed(new DriveArmPosition(IntakeShooter::kBackShot));
